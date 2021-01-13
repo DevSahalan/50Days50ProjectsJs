db4868c23b254f21562878b21d83bf5c61066cbc
sahalan hasan <mythsahalan@yahoo.com>
2021-01-14 03:33:20 +0600

    1

---
+++ 1/script.js
@@ -0,0 +1,7 @@
+const panels = document.querySelectorAll('.panel')
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    document.querySelector('.active').classList.remove('active')
+    panel.classList.add('active')
+  })
+})
